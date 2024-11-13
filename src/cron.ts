d1886eafeeba1d9aaa9a7a2739232436ce799046
Andrew Lisowski <lisowski54@gmail.com>
2024-11-12 23:05:57 -0800

    every hour delete everything older than 2 days old

--- src/cron.ts
+++ src/cron.ts
@@ -3,14 +3,14 @@ import { db } from "./lib/db.js";
 
 CronJob.from({
   start: true,
-  cronTime: "0 0 */2 * *",
+  cronTime: "0 * * * *",
   onTick: () => {
     // Delete posts older than 1 day
     const toTopOfHour = new Date().getMinutes();
     const statement = db.prepare(
       `
         DELETE FROM post
-        WHERE createdAt < STRFTIME('%Y-%m-%d %H:%M:%S', 'now', '-${toTopOfHour} minutes' '-1 day');
+        WHERE createdAt < STRFTIME('%Y-%m-%d %H:%M:%S', 'now', '-${toTopOfHour} minutes' '-2 day');
       `
     );
 
